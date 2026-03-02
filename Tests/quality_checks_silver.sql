df25f0608cec1b40ea0d7fc25e7492df05cb7318
Khaled Miri <mirikhaled0@gmail.com>
2026-03-02 16:27:19 +0100

    Update quality_checks_silver.sql

--- Tests/quality_checks_silver.sql
+++ Tests/quality_checks_silver.sql
@@ -1,1 +1,56 @@
+/*
+===============================================================================
+Quality Checks
+===============================================================================
+Script Purpose:
+    This script performs various quality checks for data consistency, accuracy, 
+    and standardization across the 'silver' layer. It includes checks for:
+    - Null or duplicate primary keys.
+    - Unwanted spaces in string fields.
+    - Data standardization and consistency.
+    - Invalid date ranges and orders.
+    - Data consistency between related fields.
 
+Usage Notes:
+    - Run these checks after data loading Silver Layer.
+    - Investigate and resolve any discrepancies found during the checks.
+===============================================================================
+*/
+
+--Check for primary key redundancy or if it is NULL
+SELECT prd_id, COUNT(*) 
+FROM silver.crm_prd_info
+GROUP BY prd_id
+HAVING COUNT(*)>1 OR prd_id IS NULL
+
+--Check for unwanted spaces 
+SELECT prd_nm 
+FROM silver.crm_prd_info
+WHERE prd_nm != TRIM(prd_nm)
+
+--Check for NULLs or negative values
+SELECT prd_cost
+FROM silver.crm_prd_info
+WHERE prd_cost < 0 OR prd_cost IS NULL
+
+--Check for Data standardization and consistency
+SELECT DISTINCT prd_line
+FROM silver.crm_prd_info
+
+--Check for invalid date orders
+SELECT * FROM bronze.crm_sales_details
+WHERE sls_order_dt > sls_ship_dt OR sls_order_dt > sls_due_dt OR sls_ship_dt > sls_due_dt
+
+
+--Check for Data Consistency: Between sales, quantity and price
+-->> Sales = quantity * price
+--> Values must not be NULL or 0 and must be positive
+
+SELECT
+sls_sales,
+sls_quantity,
+sls_price
+FROM bronze.crm_sales_details
+WHERE sls_sales != sls_quantity * sls_price 
+OR sls_sales IS NULL OR sls_quantity IS NULL OR sls_price IS NULL
+OR sls_sales <=0 OR sls_quantity <=0 OR sls_price <=0
